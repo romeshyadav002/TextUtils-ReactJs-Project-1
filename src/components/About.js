a1600da6292160f1ec8b895af5b4d76b68bb95ad
ROMESH YADAV <88088188+romeshyadav009@users.noreply.github.com>
2023-04-16 00:09:30 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,110 @@
+import React from "react";
+
+export default function About(props) {
+  // const [myStyle, setMyStyle] = useState({
+  //     color: 'black',
+  //     backgroundColor: 'white'
+  // })
+  let myStyle = {
+    color: props.mode === "dark" ? "white" : "#042743",
+    backgroundColor: props.mode === "dark" ? "rgb(36 74 104)" : "white",
+  };
+
+  return (
+    <div className="container">
+      <h1
+        className="my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        About Us
+      </h1>
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button
+              className="accordion-button"
+              type="button"
+              style={myStyle}
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+              <strong>Analyze Your text </strong>
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              Textutils gives you a way to analyze your text quickly and
+              efficiently. Be it word count, character count or copy text or
+              convert cases of characters.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button
+              className="accordion-button collapsed"
+              style={myStyle}
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              <strong>Free to use </strong>
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              TextUtils is a no-cost utility that offers immediate character and
+              word count statistics for any text you input. It's a practical
+              tool for composing texts with specific character or word
+              limitations, as TextUtils provides accurate reports of both the
+              number of words and characters.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingThree">
+            <button
+              className="accordion-button collapsed"
+              style={myStyle}
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              <strong>Browser Compatible </strong>
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              This software for counting words is compatible with all major web
+              browsers, including Chrome, Firefox, Internet Explorer, Safari,
+              and Opera. Its versatility makes it a useful tool for calculating
+              character counts in a variety of contexts, including Facebook,
+              blogs, books, Excel documents, PDFs, and essays, among others.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
